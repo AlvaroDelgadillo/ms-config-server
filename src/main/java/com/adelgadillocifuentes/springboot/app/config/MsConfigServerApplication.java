1f01e88e741497350998ed687310d4b72069c47b
Alvaro Delgadillo Cifuentes <adelgadillocifuentes@gmail.com>
2020-05-30 00:26:17 -0500

    Started Project

---
+++ src/main/java/com/adelgadillocifuentes/springboot/app/config/MsConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.adelgadillocifuentes.springboot.app.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsConfigServerApplication.class, args);
+	}
+
+}
